de355788e04bd5cb8bae373b616e38d6a05c27a4
FarfanColin <cjfc12345@hotmail.com>
2020-10-23 18:51:54 +0100

    2310

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -14,10 +14,29 @@ const commentRouter = express.Router();
 function router() {
   commentRouter.route('/comment')
     .post((req, res) => {
-      debug(req.body);
-      req.login(req.body, ()=>{
-         res.redirect('/comment/comment'); 
-      })
+        const { commentname} = req.body;
+        const uri = process.env.MONGO_URI;
+        const dbname = 'CA';
+
+        (async function addcomment() {
+            let client;
+            try{
+                client = await MongoClient.connect(uri);
+                debug('Connected correctly to server');
+                
+                const db = client.db(dbname);
+
+                const col = db.collection('comments');
+                const comment = { commentname};
+                const results = await col.insertOne(comment);
+                debug(results);
+                req.login(results.ops[0], ()=>{
+                    res.redirect('/comment/comment'); 
+                });
+            } catch(err){
+                debug(err);
+            }
+        }());
     });
     commentRouter.route('/comment')
     .get((req, res)=>{
